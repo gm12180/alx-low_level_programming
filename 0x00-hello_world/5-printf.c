0752c22525383fa380b1cfa0b2ccae3b6e9d062b
gm12180 <lesleymukami@gmail.com>
2022-06-02 13:50:43 -0700

    tsk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-/*simple c program */
-int main()
+/*simple c program 
+ * main; entry point
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art");
 return (0);
