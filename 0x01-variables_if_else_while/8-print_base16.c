d4d264837e764d425f60a918fddc2eb274899643
gm12180 <lesleymukami@gmail.com>
2022-06-06 16:29:39 -0700

    tsk8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+char x;
+int y;
+
+for (y = 0; y <= 9; y++)
+{
+	putchar(y);
+}
+for (x = 'a'; x <= 'f'; x++)
+{
+	putchar(x);
+}
+putchar('\n');
+}
