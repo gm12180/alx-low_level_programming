227d25e4257770a3764c556be2831bc226d4ca8b
gm12180 <lesleymukami@gmail.com>
2022-06-07 14:29:56 -0700

    tsk2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints alphabt 10 times
+ *
+ * Return: No return value
+ */
+void print_alphabet_x10(void)
+{
+int y;
+char x;
+
+for (y = 0;  y <= 10; y++)
+{
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(y);
+	}
+}
+_putchar('\n');
+
+}
+
