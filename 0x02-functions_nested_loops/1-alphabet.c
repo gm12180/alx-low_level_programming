a9eea50aed4b179e28f3d8c3b5eb963661850f30
gm12180 <lesleymukami@gmail.com>
2022-06-07 13:45:14 -0700

    tsk1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *
+ * print_alphabet - print alphabets in lowercase
+ * 
+ * Return: No return value
+ */
+void print_alphabet(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+	_putchar(x);
+}
+_putchar('\n');
+
+}
