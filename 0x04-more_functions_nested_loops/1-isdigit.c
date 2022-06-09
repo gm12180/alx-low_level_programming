936960870ba47be6e58f9a2f8ef250fe24adb86b
gm12180 <lesleymukami@gmail.com>
2022-06-09 06:27:05 -0700

    tsk3

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,19 @@
 #include "main.h"
 /**
- * _isdigit - a fuction that checks if input is a digit
- * @c: integer c
- *
- * Return: Return 1 if true, else return 0
+ *  * _isdigit - if is a digit
+ *   *@c: print int
+ *    * Return: Always 0.
  */
+
 int _isdigit(int c)
 {
-if (c >= 0 && c <= 9)
-{
-	return (1);
-}
-else
-	return (0);
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints the numbers
+ *
+ * Return: Nothing to return
+ */
+void print_numbers(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+	_putchar (x);
+}
+_putchar ('\n');
+}
