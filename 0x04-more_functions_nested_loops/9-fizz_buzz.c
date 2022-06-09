3bf42b07b823b76870241070404cccab2bde0759
gm12180 <lesleymukami@gmail.com>
2022-06-09 12:39:45 -0700

    tsl9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+for (x = 1; x <= 100; x++)
+{
+	if (x % 3 == 0)
+	{
+		printf("Fizz");
+		printf(" ");
+	}
+	else if (x % 5 == 0)
+	{
+		printf("Buzz");
+		printf(" ");
+	}
+	else if (x % 3 == 0 && x % 5 == 0)
+	{
+		printf("FizzBuzz");
+		printf(" ");
+	}
+	else
+	{
+		printf("%d", x);
+		printf(" ");
+	}
+	}
+printf("\n");
+return (0);
+
+}
