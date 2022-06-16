d5eaed1907ba9b93d9ebf663c3300fe2cde57089
gm12180 <lesleymukami@gmail.com>
2022-06-16 11:01:14 -0700

    t0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: variable for first string
+ * @src: variable for 2nd string
+ * Return: Always return 0
+ */
+char *_strcat(char *dest, char *src)
+{
+
+return (0);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ *  * reverse_array - reverses the content of an array of integers
+ *   * @a: an array of integers
+ *    * @n: number of elements of the array
+ *     * Return: returns void
+ */
+
