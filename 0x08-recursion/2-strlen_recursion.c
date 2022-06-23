21e645083c424739fe16885cccf534d9f406be4b
gm12180 <lesleymukami@gmail.com>
2022-06-23 02:50:19 -0700

    tsl2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@ int _strlen_recursion(char *s)
 {
 int length = 0;
 
-while (s[length])
-length++;
-
-/*for (length = 0;  length != '\0'; length++)*/
+if (*s == '\0')
+return (0);
+else
 {
-_strlen_recursion(s);
+length++;
+length  = 1+_strlen_recursion(s + 1);
 return (length);
 }
 }
