bf9f7ef39f0fed8e45b390dcd725806837ba1326
gm12180 <lesleymukami@gmail.com>
2022-06-22 23:39:56 -0700

    tsk1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: character parameter
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+	{
+	_putchar(*s);
+	s--;
+	}
+_print_rev_recursion(s);
+}
