f677c87f091b58cba2267607a677d9d7115be3bb
gm12180 <lesleymukami@gmail.com>
2022-06-24 11:52:23 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function that prints the name of the program
+ *
+ * Return: return 0(success)
+ */
+int main(int argc, char *argv[])
+{
+
+}
