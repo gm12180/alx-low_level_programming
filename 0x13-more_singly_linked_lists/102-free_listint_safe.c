449309371251cb4d6d1231a41cf44f133011c5e6
gm12180 <99638960+gm12180@users.noreply.github.com>
2022-07-19 11:58:15 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,33 +8,34 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+size_t len = 0;
+int diff;
+listint_t *temp;
 
-	if (!h || !*h)
-		return (0);
+if (!h || !*h)
 
-	while (*h)
-	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
-			break;
-		}
-	}
+return (0);
 
-	*h = NULL;
+while (*h)
+{
+diff = *h - (*h)->next;
+if (diff > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
+{
+free(*h);
+*h = NULL;
+len++;
+break;
+}
+}
 
-	return (len);
+*h = NULL;
+
+return (len);
 }
